f8d9dc688784af6ab0c5b228e8e4280c622b6169
T6M <T6M>
2017-03-16 17:04:21 +0300

    Initial mainSano server

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main (String args[]) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server  = new Server(8080);
+    }
+}
